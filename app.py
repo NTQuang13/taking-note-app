b0657a004b89fd885b59bf9a09b23be234e5b813
Nguyễn Trường Quang <nguyentruongquang13@gmail.com>
2025-10-18 00:02:37 +0700

    Update app run configuration for debugging
    Change the port to 5000 and enable debug mode.

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import os
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=os.environ['PORT'])
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
